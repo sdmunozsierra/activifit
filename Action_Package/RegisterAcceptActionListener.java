fa1051d52b1e97fa14d21245610bde7bfefe8505
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-05 02:39:23 -0600

    Update RegisterAcceptActionListener.java
    Has not been tested with the actual project but I think it will work.

--- Action_Package/RegisterAcceptActionListener.java
+++ Action_Package/RegisterAcceptActionListener.java
@@ -1,67 +1,93 @@
 package action_package;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
 public class RegisterAcceptActionListener implements ActionListener {
-	private static Border border_default = BorderFactory.createLineBorder(Color.BLACK);
-	private static Border border_error = BorderFactory.createLineBorder(Color.RED);
 
 	private final JTextField[] t_input_array;
 	private final int size;
 	private final JFrame F;
 
-	public String name;
-	public int age;
-	public int weight;
-	public int height;
-	public int actId;
-	public String email;
-	
-	int invalidArgs;
-	
+	private String[] labels = { "Name: ", "Age: ", "Weight: ", "Height: ", "Active Id: ", "Email: " };
+
+	private String n; // name
+	private int a; // age
+	private int w; // weight
+	private int h; // height
+	private int id; // active Id
+	private String e; // email
+
 	// Constructor
-	public RegisterAcceptActionListener(final JTextField[] t_array, JFrame F){
-    	super();
-        this.size = t_array.length;
-    	this.t_input_array = new JTextField[size];
-        for (int i = 0; i < size; i++) {
+	public AcceptActionListener(final JTextField[] t_array, JFrame F) {
+		super();
+		this.size = t_array.length;
+		this.t_input_array = new JTextField[size];
+		for (int i = 0; i < size; i++) {
 			this.t_input_array[i] = t_array[i];
-			this.t_input_array[i].setBorder(border_default); // login text field
 		}
-        this.F = F;
-        this.invalidArgs = 0;
-    }
+		this.F = F;
+	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		fillInputs();
-		
-		if(validateName(this.name)){
-			t_input_array[0].setBorder(border_error);
-			t_input_array[0].selectAll();
-			t_input_array[0].setSelectedTextColor(Color.RED);
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Pressed a button");
+		extractData(t_input_array);
+		//I suppose the following can be done with a CASE Statement
+		if (this.a == -1) {
+			System.out.println("BAD AGE INPUT");
+		} else if (this.w == -1) {
+			System.out.println("BAD WEIGHT");
+		} else if (this.h == -1) {
+			System.out.println("BAD HEIGHT");
+		} else if (this.id == -1) {
+			System.out.println("BAD INDEX");
+		} else {
+			printData();
+			// If everything all right dispose Screen 
+			// Create a USER
+			// F.dispose();
 		}
-		
-		
+
 	}// end action performed
-	
-	 private static boolean validateName(String name){
-		 return name.matches( "[A-Z][a-zA-Z]*" );
-	 }
-	 
-	 private void fillInputs(){
-		 this.name = this.t_input_array[0].getText();
-		 this.age = Integer.parseInt(this.t_input_array[1].getText());
-		 this.weight = Integer.parseInt(this.t_input_array[2].getText());
-		 this.height = Integer.parseInt(this.t_input_array[3].getText());
-		 this.actId = Integer.parseInt(this.t_input_array[4].getText());
-		 this.email = this.t_input_array[5].getText();
-	 }
-}
+
+	// Extracts the data from the JTextField
+	public void extractData(JTextField[] data) {
+		/*
+		 * I don't know if there is a way to loop this instead of hard coding
+		 * it..?
+		 */
+		this.n = data[0].getText();
+		this.a = tryParse(data[1].getText());
+		this.w = tryParse(data[2].getText());
+		this.h = tryParse(data[3].getText());
+		this.id = tryParse(data[4].getText());
+		this.e = data[5].getText();
+	}
+
+	// Prints the data [debugging purposes]
+	public void printData() {
+		int i = 0;
+		while (i < size) {
+			System.out.println(labels[i] + ": " + t_input_array[i].getText());
+			i++;
+		}
+	}
+
+	//Method for checking if the parsed Int is really an Integer
+	public static Integer tryParse(String text) {
+		try {
+			return Integer.parseInt(text);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
+
+	/**
+	 * TO-DO: Create a method for checking the name for invalid chars
+	 * 
+	 * */
+}// end class
