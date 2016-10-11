7169d6fbf60aa385f04914058e161981a7c94ae8
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-10 23:05:20 -0600

    Create NavPanelBackActionListener.java

---
+++ Action_Package/NavPanelBackActionListener.java
@@ -0,0 +1,28 @@
+package action_package;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import activifit.Screen;
+
+/* Class for going back to the home screen */
+public class NavPanelBackActionListener implements ActionListener {
+
+	// Create private variables
+	private final JFrame F;
+	
+	//Constructor
+	public NavPanelBackActionListener(JFrame F){
+	        super();
+	        this.F = F;
+	    }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Screen.screen_home(); // open Home Screen
+		F.dispose(); // Kill window once launched the next one
+	}// end action performed
+
+}//end class
