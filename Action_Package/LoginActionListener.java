d1687f8ec2f3893af3d4a5067aaee3e6697f99b2
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-11 23:15:33 -0600

    Update LoginActionListener
    Added return valid email to be used by the Screen class at register
    field.

--- Action_Package/LoginActionListener.java
+++ Action_Package/LoginActionListener.java
@@ -15,13 +15,13 @@ public class LoginActionListener implements ActionListener{
 	private static Border border_default = BorderFactory.createLineBorder(Color.BLACK);
 	private static Border border_error = BorderFactory.createLineBorder(Color.RED);
 	
-	private final JTextField t_login;
+	private static JTextField t_login;
 	private final JFrame F;
 
     public LoginActionListener(final JTextField textField, JFrame F){
         super();
-        this.t_login = textField;
-        this.t_login.setBorder(border_default); // login text field
+        LoginActionListener.t_login = textField;
+        LoginActionListener.t_login.setBorder(border_default); // login text field
         this.F = F;
     }
 
@@ -46,11 +46,18 @@ public class LoginActionListener implements ActionListener{
 		}//end action performed
 		
 	/* --Extra Methods-- */
+	//Check for a valid email
 	public static boolean isValidEmailAddress(String email) {
 		//From http://stackoverflow.com/questions/624581/what-is-the-best-java-email-address-validation-method
 		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
 		java.util.regex.Matcher m = p.matcher(email);
 		return m.matches();
+	}
+	
+	//Return the valid email
+	public static String returnValidEmail(){
+		String valid = t_login.getText();
+		return valid;
 	}
 }//end class
