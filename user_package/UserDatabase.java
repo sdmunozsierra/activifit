853de7e49d8cbb24d6a0af22c1f784c0ca39ed44
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-14 15:20:33 -0600

    Working Database
    The database is set and it is working. So far has limited features but it has enough to continue developing the program.
    Created a method that prints the Database with all the users inside with details. (Nice feature huh).

--- user_package/UserDatabase.java
+++ user_package/UserDatabase.java
@@ -6,13 +6,13 @@ import java.util.Set;
 
 public class UserDatabase {
 	
-	Set<User> database = new HashSet<User>();
-	//User newUser;
+	//Variables
+	private Set<User> database;
 	
 	
 	//Empty User Database
 	public UserDatabase(){
-		
+		this.database = new HashSet<User>();
 	}
 	
 	//Add a user
@@ -22,19 +22,49 @@ public class UserDatabase {
 	
 	//Print all users in database
 	public void printAll(){
+		System.out.println("Users in this database:" );
+		int i = 0;
 		for (User s : database) {
-		    s.printUserName();
-		    System.out.println();
+			System.out.println("User #"+i+" "+s.getName()+" at "+s.getEmail());
+			i++;
 		}
 	}
+	
+	/* Print All with Details */
+	public void printAllDetails(){
+		//Prints the whole database with a nice format
+		System.out.println(""); //Force empty line
+		formatLine(25);
+		System.out.println("DATABASE");
+		formatLine(25);
+		int number = 1;
+		for (User s : database){
+			System.out.printf("USER #%d\n",number);
+			formatLine(25);
+			System.out.printf("Name: %s\nEmail: %s\n\nPersonal Information:\n",s.getName(),s.getEmail());
+			System.out.printf("Age: %d \nWeight %d lbs \nHeight: %d cm \nActive Id: %d \n",s.getAge(),s.getWeight(),s.getHeight(),s.getActId());
+			formatLine(25);
+			number++;
+		}//end for
+		System.out.println("END DATABASE");
+		formatLine(25);
+	}//end method
 	
 	public User getCurrentUser(){
 		//Missing check if database ! empty
 		Iterator<User> iter = database.iterator();
 		return iter.next();
 	}
-	//TO-DO
-	//Avoid duplicate users 
 	
+	/* Format for a dashed line */
+	public void formatLine(int numberOfDashes){
+		for (int i = 0; i < numberOfDashes ; i++) {
+			System.out.print("-");
+		}
+		System.out.print("\n");
+	}
+	
+	//TO-DO
+		//Avoid duplicate users 
 	
 }//end class
