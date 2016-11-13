77a33a3e366700648226e415d9ed97cb151268b3
scarrillo9 <scarrillo7@miners.utep.edu>
2016-11-13 12:56:55 -0700

    javadoc documentation

--- action_package/AboutUsActionListener.java
+++ action_package/AboutUsActionListener.java
@@ -4,18 +4,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-
+/**The action listener for clicking the About Us button
+ * that directs the user to the About Us screen.
+ * @author JSSP Engineers
+ * @version 1.0
+ */
 public class AboutUsActionListener implements ActionListener {
 
 	// Create private variables
 	private final JFrame F;
 
-	// Constructor
+	/**
+	 * 
+	 * @param F 
+	 */
 	public AboutUsActionListener(JFrame F) {
 		super();
 		this.F = F;
 	}
-
+	/**
+	 * 
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		//Show as a pop-up screen
