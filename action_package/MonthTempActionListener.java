3c1b7348ccfb0a10bdb28f56a0a7755fdde8c0ee
Javier Rivera <jrivera17@miners.utep.edu>
2016-11-17 12:53:54 -0700

    java doc

--- action_package/MonthTempActionListener.java
+++ action_package/MonthTempActionListener.java
@@ -6,13 +6,20 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 import gui_package.Screen;
-
+/**The action listener is for clicking the button to display monthly body temperature
+ * that directs the user to a graph of data.
+ * @author JSSP Engineers
+ * @version 1.0
+ */
 public class MonthTempActionListener implements ActionListener {
 	//Temp  Week Month 5,6
 
 	// Create private variables
 	private final JFrame F;
-
+    /**
+     * 
+     * @param F A JFrame to display the monthly body temperature chart
+     */
 	// Constructor
 	public MonthTempActionListener(JFrame F) {
 		super();
@@ -24,4 +31,4 @@ public class MonthTempActionListener implements ActionListener {
 		Screen.screen_customView(6); // open help
 		F.dispose(); // Kill window once launched the next one
 	}// end action performed
-}
+}//end class
