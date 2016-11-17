03c89e896bab42f66802ebd30a62bef039d51345
Javier Rivera <jrivera17@miners.utep.edu>
2016-11-17 13:02:55 -0700

    java doc

--- action_package/MonthStepsActionListener.java
+++ action_package/MonthStepsActionListener.java
@@ -6,13 +6,20 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 import gui_package.Screen;
-
+/**The action listener is for clicking the button to display monthly steps
+ * that directs the user to a graph of data.
+ * @author JSSP Engineers
+ * @version 1.0
+ */
 public class MonthStepsActionListener implements ActionListener {
 	// * Steps Week Month 3,4
 
 	// Create private variables
 	private final JFrame F;
-
+    /**
+     * 
+     * @param F A JFrame to display monthly step data
+     */
 	// Constructor
 	public MonthStepsActionListener(JFrame F) {
 		super();
@@ -24,4 +31,4 @@ public class MonthStepsActionListener implements ActionListener {
 		Screen.screen_customView(4); // open help
 		F.dispose(); // Kill window once launched the next one
 	}// end action performed
-}
+}//end class
