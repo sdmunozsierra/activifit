2162a895c15443dc3cdedf6a27c46dcbb675162c
Javier Rivera <jrivera17@miners.utep.edu>
2016-11-17 13:06:06 -0700

    java doc

--- chart_package/SleepChart.java
+++ chart_package/SleepChart.java
@@ -2,18 +2,26 @@ package chart_package;
 
 import javax.swing.JPanel;
 
-import generator_package.DataGenerator;
+import action_package.DataGenerator;
 import generator_package.Generator_Sleep;
 import gui_package.Screen;
 import javafx.stage.Stage;
-
+/**This displays and creates a sleep chart by accessing the data 
+ * @author JSSP Engineers
+ * @version 1.0
+ */
 public class SleepChart extends Charts {
 	
 	//Access the data of the current user via Generator
 	static DataGenerator localGen = Screen.globalGen;
 	
-	
-	/** Testing a drawChart from outside [Debugging] */
+	/**
+	 * 
+	 * @param opt
+	 * @param generator
+	 * @return a JPanel chart with the data inserted 
+	 */
+	/* Testing a drawChart from outside [Debugging] */
 	public static JPanel drawChart(int opt, Generator_Sleep generator){
 		//Data will be manipulated in order to fit the Sleep criteria
 		int[][] data = generator.getRandomData();
