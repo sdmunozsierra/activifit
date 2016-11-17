dc631d0297ee3f2ec672c6473fb4ed78a9f1267c
Javier Rivera <jrivera17@miners.utep.edu>
2016-11-17 12:45:16 -0700

    conflict

---
+++ action_package/LoginHelpActionListener.java
@@ -0,0 +1,37 @@
+package action_package;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import gui_package.Screen;
+/**The action listener for clicking the help button from the login screen
+ * that directs the user to the help screen.
+ * @author JSSP Engineers
+ * @version 1.0
+ */
+public class LoginHelpActionListener implements ActionListener {
+
+	// Create private variables
+	private final JFrame F;
+
+	// Constructor
+	/**
+	 * 
+	 * @param F
+	 */
+	public LoginHelpActionListener(JFrame F) {
+		super();
+		this.F = F;
+	}
+    
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		/* Should check if it is going to redirect to the same help
+		 * screen, or if it has to be a specialized one */
+		Screen.screen_help(); // open help screen
+		//F.dispose(); // This will be disabled because help screen is only a popout
+	}// end action performed
+
+}// end class
