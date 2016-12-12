2d688e81c10161ae848479da805db7cc4d21384e
scarrillo9 <scarrillo7@miners.utep.edu>
2016-12-12 07:11:09 -0700

    created javadoc for weather generator -stef

--- generator_package/WeatherGenerator.java
+++ generator_package/WeatherGenerator.java
@@ -7,83 +7,103 @@ import javax.swing.JPanel;
 
 import gui_package.CustomJPanels;
 import gui_package.Screen;
-
+/**
+ * Generator for weather home page information.
+ * @author JSSP Engineers
+ * @version 1.0
+ * 
+ */
 public class WeatherGenerator {
-	//TODO JavaDocs
-	// Height and Width of the icons
 	private static int height = 40;
 	private static int width = 40; 
 	private static String status = "";
 
-	public WeatherGenerator(){
-		
-	}
+	/**
+	 * Empty constructor
+	 */
+	public WeatherGenerator(){}
 	
 	
-	//Get the status of the weather
+	/**
+	 * Method receives the status of the weather to print on the aoo's home page
+	 * @return String status 
+	 */
 	public static String getStatus() {
 		return status;
 	}
 
-	//Generates the weather
+	/**
+	 * Method randomly chooses a weather to display on the app for aesthetic purposes
+	 * @return JLabel with the picture of the weather
+	 */
 	public static JLabel generateWeather(){
 		//Case 1 currently unavailable
-		int generator = ThreadLocalRandom.current().nextInt(2,7);
+		int generator = ThreadLocalRandom.current().nextInt(1,6);
 		//7 not inclusive
 		switch (generator){
-//		case 1:
-//			return 1;
-		//TODO Add day and night
-		case 2:
+		case 1:
 			status = "Cloudy";
 			return cloudy();
-		case 3:
+		case 2:
 			status = "Partly Cloudy";
 			return partlyCloudy();
-		case 4:
+		case 3:
 			status = "Rainy";
 			return rainy();
-		case 5:
+		case 4:
 			status = "Snowy";
 			return snowy();
-		case 6:
+		case 5:
 			status = "Thunder Storm";
 			return thunderstorm();
 		}
 		return null; 
 	}
 	
-	
-	private static JPanel dayNight(){
-		//depending on time will send day or night
-		JPanel background = CustomJPanels.backgroundPanel("", 3);
-		return background;
-	}//end day or night panel
-	
+	/**
+	 * Method creates the cloudy weather
+	 * @return JLabel with picture
+	 */
 	private static JLabel cloudy(){
 		//JPanel background = CustomJPanels.backgroundPanel("https://builder.impress.ly/api/image/43341a3c-3c37-11e5-80d0-00155d130a43", 3);
 		JLabel cloudy = Screen.getWebIconScaledLabel("http://i.imgur.com/etQQpsS.png", height, width);
 		return cloudy;
 	}//end cloudy panel
 	
+	/**
+	 * Method creates the partly cloudy weather
+	 * @return JLabel with picture
+	 */
 	private static JLabel partlyCloudy(){
 		//JPanel background = CustomJPanels.backgroundPanel("https://www.shareicon.net/data/128x128/2015/12/01/680819_cloud_512x512.png", 3);
 		JLabel partCloudy = Screen.getWebIconScaledLabel("https://www.shareicon.net/data/128x128/2015/12/01/680819_cloud_512x512.png", height, width);
 		return partCloudy;
 	}//end partly cloudy panel
 	
+	/**
+	 * Method creates the rainy weather
+	 * @return JLabel with picture
+	 */
 	private static JLabel rainy(){
 		//JPanel background = CustomJPanels.backgroundPanel("http://iconshow.me/media/images/ui/ios7-icons/png/128/rainy-outline.png", 3);
 		JLabel rainy = Screen.getWebIconScaledLabel("http://iconshow.me/media/images/ui/ios7-icons/png/128/rainy-outline.png", height, width);
 		return rainy;
 	}//end rainy panel
 	
+	/**
+	 * Method creates the snowy weather
+	 * @return JLabel with picture
+	 */
 	private static JLabel snowy(){
 		//JPanel background = CustomJPanels.backgroundPanel("https://www.shareicon.net/data/128x128/2015/12/01/680565_cloud_512x512.png", 3);
 		JLabel rainy = Screen.getWebIconScaledLabel("https://www.shareicon.net/data/128x128/2015/12/01/680565_cloud_512x512.png", height, width);
 		return rainy;
 	}//end snowy panel
 	
+	/**
+	 * Method creates the thunderstorm weather
+	 * @return JLabel with picture
+	 */
 	private static JLabel thunderstorm(){
 		//JPanel background = CustomJPanels.backgroundPanel("http://www.uidownload.com/files/640/502/468/bolt-cloud-forecast-lightning-storm-weather-icon.png", 3);
 		JLabel thunder = Screen.getWebIconScaledLabel("http://www.uidownload.com/files/640/502/468/bolt-cloud-forecast-lightning-storm-weather-icon.png", height, width);
